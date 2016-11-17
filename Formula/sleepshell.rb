9b838dd6a193d8a791e9634b784d5ab56edb7aaf
Jason Weathered <jason@jasoncodes.com>
2016-11-17 15:28:10 +1000

    Update to SHA-256 checksums

--- Formula/sleepshell.rb
+++ Formula/sleepshell.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Sleepshell < Formula
   url 'http://www.mariovaldez.net/software/sleepshell/files/sleepshell_0.0.2.tar.gz'
   homepage 'http://www.mariovaldez.net/software/sleepshell/'
-  sha1 '8850472d76be617517a472d1399bc01ba7d4eeec'
+  sha256 '6a5ca129cc01971341bcfe86ebb9ba750f6d004a7a2262b8685e7a036b30fac0'
 
   def install
     system "make"
